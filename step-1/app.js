43ff45fac5f9dfd7361e2699cb66eb9321f845c1
Adam Tirella <adam@tirel.la>
2023-06-27 18:00:58 -0700

    Split demo into steps

---
+++ step-1/app.js
@@ -0,0 +1,50 @@
+/**
+ *
+ * RESOURCES
+ *
+ **/
+
+/** Declare element variables */
+const toggleModalEl = document.getElementById("toggle-modal");
+const modalEl = document.getElementById("modal");
+
+/** Not Map Things */
+function handleModalChange() {
+  modalEl.open = !modalEl.open;
+}
+
+/** Layer Resources */
+const apiKey =
+  "AAPK930915ff5f15456dbbfa77b4ae41c180GXXMr_3eIL9DvdQHcFVasjxNKfbzymMyo6L9N9JGnYvkyP1myvnevJmBoFYLf2DB";
+
+/**
+ *
+ * MAPS SDK
+ *
+ **/
+import esriConfig from "https://js.arcgis.com/4.27/@arcgis/core/config.js";
+import Map from "https://js.arcgis.com/4.27/@arcgis/core/Map.js";
+import MapView from "https://js.arcgis.com/4.27/@arcgis/core/views/MapView.js";
+import Home from "https://js.arcgis.com/4.27/@arcgis/core/widgets/Home.js";
+import Locate from "https://js.arcgis.com/4.27/@arcgis/core/widgets/Locate.js";
+
+esriConfig.portalUrl = "https://jsapi.maps.arcgis.com/";
+esriConfig.apiKey = apiKey;
+toggleModalEl.addEventListener("click", () => handleModalChange());
+
+const map = new Map({
+  basemap: "streets-night-vector",
+});
+
+const view = new MapView({
+  container: "viewDiv",
+  map: map,
+  center: [-100, 45],
+  zoom: 3,
+});
+
+const locateBtn = new Locate({ view: view });
+const homeWidget = new Home({ view: view });
+
+view.ui.add(homeWidget, "top-left");
+view.ui.add(locateBtn, { position: "top-left" });
